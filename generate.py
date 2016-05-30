1d6b1412e2fcce104c8410c9ea7f73e6a01d5b45
Stas Kolodyuk <staskolodyuk@gmail.com>
2016-05-30 11:41:14 +0300

    Fix Whitespace

--- generate.py
+++ generate.py
@@ -3,7 +3,7 @@ import wikipedia
 
 for i in range(1, int(sys.argv[1])):
     page = wikipedia.page(wikipedia.random())
-    print "Creating " + page.title.encode("utf-8") + ".txt ..."
-    with open("input/" + page.title.encode("utf-8") + ".txt", "a+") as file:
+    print "Creating " + page.title.encode("utf-8").replace(" ", "") + ".txt ..."
+    with open("input/" + page.title.encode("utf-8").replace(" ", "") + ".txt", "a+") as file:
         file.write(page.title.encode("utf-8") + "\n")
         file.write(page.content.encode("utf-8"))
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 hadoop fs -rm -r output
 hadoop fs -rm -r input
 hadoop fs -mkdir input
-hadoop fs -copyFromLocal input/* input
+hadoop fs -copyFromLocal input/* input/
 hadoop jar target/inverted-index-1.0-SNAPSHOT.jar input output
 clear
-hadoop fs -cat output/*
+hadoop fs -cat output/* >> output.txt
